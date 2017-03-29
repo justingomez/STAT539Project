9436a3d5c0d16c8b67f6f6ca1155cb652cc8671f
Your Name <you@example.com>
2017-03-29 13:31:13 -0600

    i hate sand it gets everywhere

--- analyses.R
+++ analyses.R
@@ -1,1 +1,30 @@
 train<-read.csv("train.csv",header=TRUE)[,-c(1:2)]
+
+View(train)
+
+if(!require(psych)){install.packages("psych")}
+  library(psych)
+
+pairs.panels(train[,10:ncol(train)])
+
+#correlations
+library(corrplot)
+
+
+#initial data analysis
+apply(train[,c(2:5,10:ncol(train))],2, summary)
+
+#raw scores
+
+
+#differentials
+plot(train$astdiff, train$winloss)
+cut.diff <- cut(train$astdiff,breaks = 10)
+
+sum()
+
+
+
+
+
+
